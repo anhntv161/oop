b8d072e8a06fdeac441fa3596f5646ceff6d7a8a
vananh <22028128@vnu.edu.vn>
2023-10-08 21:13:36 +0700

    int

--- demo/src/main/java/com/example/demo/HelloController.java
+++ demo/src/main/java/com/example/demo/HelloController.java
@@ -6,11 +6,11 @@ import javafx.scene.control.Label;
 public class HelloController {
     @FXML
     private Label welcomeText;
+    private int res = 0;
 
     @FXML
     protected void onHelloButtonClick() {
         welcomeText.setText("Welcome to JavaFX Application!");
     }
-    /** comment. */
 
 }
